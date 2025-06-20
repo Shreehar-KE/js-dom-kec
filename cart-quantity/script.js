94e74079b85dbe823292a2b042ff5397f345b669
Shreehar-KE <shreehar.elancheran@gmail.com>
2025-06-20 15:13:26 +0530

    update cart-quantity project

--- cart-quantity/script.js
+++ cart-quantity/script.js
@@ -3,11 +3,36 @@
 let cartQuantity = Number(localStorage.getItem("cartQuantity"));
 
 if (!cartQuantity) {
-    localStorage.setItem("cartQuantity", 0);
-    cartQuantity = 0;
+  localStorage.setItem("cartQuantity", 0);
+  cartQuantity = 0;
 }
 // cartQuantity = cartQuantity || 0;
 
+const displayButton = document.getElementById("display-button");
+const plusOneButton = document.getElementById("plus-one");
+const plusTwoButton = document.getElementById("plus-two");
+const plusThreeButton = document.getElementById("plus-three");
+const resetButton = document.getElementById("reset");
+
+displayButton.addEventListener("click", () => {
+  displayQuantity();
+});
+plusOneButton.addEventListener("click", () => {
+  updateQuantity(1);
+  displayQuantity();
+});
+plusTwoButton.addEventListener("click", () => {
+  updateQuantity(2);
+  displayQuantity();
+});
+plusTwoButton.addEventListener("click", () => {
+  updateQuantity(3);
+  displayQuantity();
+});
+resetButton.addEventListener('click', ()=> {
+  resetCart();
+  displayQuantity();
+})
 
 function updateQuantity(value) {
   cartQuantity = cartQuantity + value;
@@ -23,8 +48,6 @@ function resetCart() {
   localStorage.setItem("cartQuantity", cartQuantity);
   console.log("The cart has been reset");
 }
-
-
 
 // camelCase
 // PascalCase
