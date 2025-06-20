f4816f5ae3ff3a383f9bb047d2929c492d227f6c
Shreehar-KE <shreehar.elancheran@gmail.com>
2025-06-20 12:18:32 +0500

    complete advanced operators

--- script.js
+++ script.js
@@ -8,24 +8,56 @@
 // newObject.display();
 
 // Console Object Methods
-console.log("Hello JS!");
-console.warn("This is a warning");
-console.error("Something went wrong!");
+// console.log("Hello JS!");
+// console.warn("This is a warning");
+// console.error("Something went wrong!");
 
-console.table([{ name: "Alice" }, { name: "Bob" }]);
+// console.table([{ name: "Alice" }, { name: "Bob" }]);
 
-console.time("loop");
-for (let i = 0; i < 1000000; i++) {}
-console.timeEnd("loop");
+// console.time("loop");
+// for (let i = 0; i < 1000000; i++) {}
+// console.timeEnd("loop");
 
-// eval() function - don't use in live projects
-const x = 10;
-const y = 20;
-const result = eval("x + y");
-console.log(result);
+// // eval() function - don't use in live projects
+// const x = 10;
+// const y = 20;
+// const result = eval("x + y");
+// console.log(result);
 
 
-// Strict Mode
-"use strict";
-let a = 10;
-b = 20;
+// // Strict Mode
+// "use strict";
+// let a = 10;
+// b = 20;
+
+
+// Ternary Operator (? :)
+// condition ? if_logic : else_logic
+
+// if (condition) {
+//     if_logic
+// } else {
+//     else_logic
+// }
+
+let greatestValue = (1 > 2) ? 1 : 2;
+console.log(greatestValue);
+
+
+// && - guard operator
+let a = (1 > 2) && 5;
+console.log(a);
+
+// if (condition) {
+//     if_logic
+// } 
+
+// || - default operator
+let input;
+let b = input || 4;
+console.log(b);
+
+
+// falsy values
+
+// 0 '' NaN undefined null 
