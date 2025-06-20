41b5d6488c21bcd1ed24589c8a018979cf0cb83b
Shreehar-KE <shreehar.elancheran@gmail.com>
2025-06-20 10:07:43 -0700

    complete console object methods

---
+++ script.js
@@ -0,0 +1,21 @@
+// let newObject = {
+//     name: "John Doe",
+//     display: function () {
+//         console.log("Hello");
+//     }
+// }
+
+
+// newObject.display();
+
+// Console Object Methods
+console.log("Hello JS!");
+console.warn("This is a warning");
+console.error("Something went wrong!");
+
+console.table([{ name: "Alice" }, { name: "Bob" }]);
+
+console.time("loop");
+for (let i = 0; i < 1000000; i++) { }
+console.timeEnd("loop");
+
