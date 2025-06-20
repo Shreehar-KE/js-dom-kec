e10b2f51af148e6ebff49fb028bb3497044be37c
Shreehar-KE <shreehar.elancheran@gmail.com>
2025-06-20 12:30:45 +0500

    complete cart-quantity

---
+++ cart-quantity/script.js
@@ -0,0 +1,32 @@
+// console.log("Hello");
+
+let cartQuantity = Number(localStorage.getItem("cartQuantity"));
+
+if (!cartQuantity) {
+    localStorage.setItem("cartQuantity", 0);
+    cartQuantity = 0;
+}
+// cartQuantity = cartQuantity || 0;
+
+
+function updateQuantity(value) {
+  cartQuantity = cartQuantity + value;
+  localStorage.setItem("cartQuantity", cartQuantity);
+}
+
+function displayQuantity() {
+  console.log(`Cart: ${cartQuantity}`);
+}
+
+function resetCart() {
+  cartQuantity = 0;
+  localStorage.setItem("cartQuantity", cartQuantity);
+  console.log("The cart has been reset");
+}
+
+
+
+// camelCase
+// PascalCase
+// kebab-case
+// snake_case
