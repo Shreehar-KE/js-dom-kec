f2c4e98207d65a4c7c5c275482822969dedf8a83
Shreehar-KE <shreehar.elancheran@gmail.com>
2025-06-20 10:10:33 -0700

    complete eval function

--- script.js
+++ script.js
@@ -5,17 +5,22 @@
 //     }
 // }
 
-
 // newObject.display();
 
 // Console Object Methods
-console.log("Hello JS!");
-console.warn("This is a warning");
-console.error("Something went wrong!");
+// console.log("Hello JS!");
+// console.warn("This is a warning");
+// console.error("Something went wrong!");
 
-console.table([{ name: "Alice" }, { name: "Bob" }]);
+// console.table([{ name: "Alice" }, { name: "Bob" }]);
 
-console.time("loop");
-for (let i = 0; i < 1000000; i++) { }
-console.timeEnd("loop");
+// console.time("loop");
+// for (let i = 0; i < 1000000; i++) {}
+// console.timeEnd("loop");
+
+// eval() function - don't use in live projects
+const x = 10;
+const y = 20;
+const result = eval("x + y");
+console.log(result);
 
